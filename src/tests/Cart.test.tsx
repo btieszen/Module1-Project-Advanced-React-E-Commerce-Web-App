4c6b4a7ef5378629897acabbd7890045ed3a7b41
btieszen <briantieszen4@gmail.com>
2025-04-21 13:46:57 -0700

    project1

--- src/tests/Cart.test.tsx
+++ src/tests/Cart.test.tsx
@@ -1,14 +1,15 @@
 import {render,fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Cart from '../pages/Cart';
+import { CartProvider } from '../context/CartContext';
 
 test('should interact and assert component interactions', () => {
     const { getByText, getByRole } = render(
-        <>
+        <CartProvider>
             <Cart />
-        </>
+        </CartProvider>
     );
-    const buttonElement = getByRole('Remove');
+    const buttonElement = getByRole('button', { name: /checkout/i });
     fireEvent.click(buttonElement);
     
     const messageElement = getByText('Your cart is empty');
